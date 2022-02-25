11f2a930d6688a8682bc49836f9bf974f74426b9
tarek doha <tarikdoha80@gmail.com>
2022-02-25 02:23:38 +0100

    Add Content Types Blog Listing and Blog Page

---
+++ Cms12.Core/Pages/BlogPage.cs
@@ -0,0 +1,50 @@
+﻿using Cms12.Core.Pages.Base;
+using Cms12.Core.Resources;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cms12.Core.Pages
+{
+    [ContentType(
+        DisplayName = "Blog Page",
+        GUID = "032f5caa-f3f3-4802-8b5d-984831afffbd",
+        GroupName = GlobalConstants.GroupName.Blog
+        )]
+    public class BlogPage : SeoMetaData
+    {
+        [Display(
+            Name = "Title",
+            GroupName = SystemTabNames.Content,
+            Order =50
+            )]
+        public virtual string Title { get; set; }
+
+        [Display(
+            Name = "Sub Title",
+            GroupName = SystemTabNames.Content,
+            Order = 100
+            )]
+        public virtual string SubTitle { get; set; }
+
+        [Display(
+            Name= "Content",
+            GroupName = SystemTabNames.Content,
+            Order = 150
+            )]
+        public virtual XhtmlString Content { get; set; }
+
+        [Display(
+            Name="Blog Image",
+            GroupName = SystemTabNames.Content,
+            Order = 200
+            )]
+        public virtual ContentReference BlogImage { get; set; }
+    }
+}
--- Cms12.Core/Pages/Homepage.cs
+++ Cms12.Core/Pages/Homepage.cs
@@ -1,4 +1,6 @@
-﻿using EPiServer.Core;
+﻿using Cms12.Core.Pages.Base;
+using Cms12.Core.Resources;
+using EPiServer.Core;
 using EPiServer.DataAbstraction;
 using EPiServer.DataAnnotations;
 using System;
@@ -10,10 +12,15 @@ using System.Threading.Tasks;
 
 namespace Cms12.Core.Pages
 {
-    [ContentType(GUID = "0b6d46cf-9676-40e4-a292-55ebd0b65313", DisplayName ="Homepage")]
-    public class Homepage: PageData
+    [ContentType(
+        GUID = "0b6d46cf-9676-40e4-a292-55ebd0b65313", 
+        DisplayName ="Home Page", 
+        GroupName =GlobalConstants.GroupName.Common)]
+    [AvailableContentTypes(Include = new [] {typeof(BlogListingPage)})]
+    [ImageUrl("/images/pic01.jpg")]
+    public class Homepage : SeoMetaData
     {
         [Display(Order = 100, GroupName = SystemTabNames.Content)]
-        public virtual ContentArea Main { get; set; }
+        public virtual ContentArea MainContentArea { get; set; }
     }
 }
